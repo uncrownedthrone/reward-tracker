ab858532a810ef02b3e6c72957d1975530416428
Justin Swiger <justin.swiger@me.com>
2020-01-11 18:51:28 -0500

    updated teacherpagecomp to buttons

--- src/components/TeacherPageComp.jsx
+++ src/components/TeacherPageComp.jsx
@@ -5,15 +5,25 @@ const TeacherPage = () => {
     <>
       <section>
         <h2>Professor McGonagall - McGonagall's Moola</h2>
-        <ul>
-          <li className="teacherPageLi">Period 1 - Transfiguration 1</li>
-          <li className="teacherPageLi">Period 2 - Transfiguration 1</li>
-          <li className="teacherPageLi">Period 3 - Transfiguration 1</li>
-          <li className="teacherPageLi">Period 5 - Transfiguration 1 Honors</li>
-          <li className="teacherPageLi">Period 6 - Transfiguration 1 Honors</li>
-          <li className="teacherPageLi">Period 8 - Transfiguration 1 Honors</li>
-        </ul>
-        <button>Add New Period +</button>
+        <button className="teacherPageButton">
+          Period 1 - Transfiguration 1
+        </button>
+        <button className="teacherPageButton">
+          Period 2 - Transfiguration 1
+        </button>
+        <button className="teacherPageButton">
+          Period 3 - Transfiguration 1
+        </button>
+        <button className="teacherPageButton">
+          Period 5 - Transfiguration 1 Honors
+        </button>
+        <button className="teacherPageButton">
+          Period 6 - Transfiguration 1 Honors
+        </button>
+        <button className="teacherPageButton">
+          Period 8 - Transfiguration 1 Honors
+        </button>
+        <button className="teacherPageButton">Add New Period +</button>
       </section>
     </>
   )
