36934a9a3f62db03727831a052c370bffd177f7b
Justin Swiger <justin.swiger@me.com>
2020-01-14 12:09:46 -0500

    collapsible box working!

--- src/components/StudentComp.jsx
+++ src/components/StudentComp.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 
 const StudentComp = () => {
-  const test = () => {
-    window.location.reload(false)
-  }
-
   return (
     <>
-      <section onClick={test} className="studentComp">
-        student name | student total
+      <section class="wrap-collabsible">
+        <input id="collapsible" className="toggle" type="checkbox" />
+        <label for="collapsible" className="lbl-toggle">
+          STUDENT
+        </label>
+        <div class="collapsible-content">
+          <div class="content-inner">
+            <p>
+              QUnit is by calling one of the object that are embedded in
+              JavaScript, and faster JavaScript program could also used with its
+              elegant, well documented, and functional programming using JS,
+              HTML pages Modernizr is a popular browsers without plug-ins.
+              Test-Driven Development.
+            </p>
+          </div>
+        </div>
       </section>
     </>
   )
