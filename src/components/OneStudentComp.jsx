0ec48d3be0a5622f6d7947a21638c27c415864f5
Justin Swiger <justin.swiger@me.com>
2020-01-17 12:18:23 -0500

    getting students from db

--- src/components/OneStudentComp.jsx
+++ src/components/OneStudentComp.jsx
@@ -1,21 +1,17 @@
 import React from 'react'
 
-const StudentPageComp = () => {
+const OneStudentComp = props => {
   return (
     <>
       <section>
-        <h2>Harry Potter -- 55 Hogwarts Points</h2>
+        <h2>NAME - {props.name}</h2>
+        <p>HOUSE - {props.house}</p>
         <ul className="studentPageUl">
           <li className="studentPageLi">Period 1 - 10 McGonagall's Moola</li>
-          <li className="studentPageLi">Period 2 - 5 Snape's Stock</li>
-          <li className="studentPageLi">Period 3 - 5 Hagrid's Half Sheets</li>
-          <li className="studentPageLi">Period 5 - 10 Sprout's Shillings</li>
-          <li className="studentPageLi">Period 6 - 10 Flitwick's Fivers</li>
-          <li className="studentPageLi">Period 8 - 15 Moody's Millions</li>
         </ul>
       </section>
     </>
   )
 }
 
-export default StudentPageComp
+export default OneStudentComp
--- src/pages/PeriodPage.jsx
+++ src/pages/PeriodPage.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import PeriodPageComp from '../components/PeriodPageComp'
 import axios from 'axios'
+import OneStudentComp from '../components/OneStudentComp'
 
 const PeriodPage = props => {
   const [period, setPeriod] = useState({})
+  const [student, setStudent] = useState([])
 
   const getPeriod = async () => {
     const resp = await axios.get(
@@ -12,8 +14,15 @@ const PeriodPage = props => {
     setPeriod(resp.data)
   }
 
+  const getStudents = async () => {
+    const resp = await axios.get('https://localhost:5001/api/Student')
+    setStudent(resp.data)
+    console.log(resp.data)
+  }
+
   useEffect(() => {
     getPeriod()
+    getStudents()
   }, [])
 
   return (
@@ -22,6 +31,17 @@ const PeriodPage = props => {
         periodNumber={period.periodNumber}
         subject={period.subject}
       />
+      <ul>
+        {student.map((student, i) => {
+          return (
+            <>
+              <li>
+                <OneStudentComp name={student.name} house={student.house} />
+              </li>
+            </>
+          )
+        })}
+      </ul>
     </div>
   )
 }
