7265cdf33ad954199d34359678a6b96cf31ba84d
Justin Swiger <justin.swiger@me.com>
2020-01-18 10:01:40 -0500

    all students showing - not assigned to period

--- src/components/StudentPageComp.jsx
+++ src/components/StudentPageComp.jsx
@@ -7,15 +7,18 @@ const StudentPageComp = () => {
   const getStudents = async () => {
     const resp = await axios.get('https://localhost:5001/api/Student')
     setStudent(resp.data)
-    console.log(resp.data)
   }
 
   useEffect(() => {
     getStudents()
   }, [])
 
-  return students.map(student => {
-    return <p>{student.name}</p>
+  return students.map((student, i) => {
+    return (
+      <p key={i}>
+        {student.name} - {student.house}
+      </p>
+    )
   })
 }
 
