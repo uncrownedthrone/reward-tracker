39ce2af284e3f73c3b7dcd334923382f21a576b9
Justin Swiger <justin.swiger@me.com>
2020-01-18 17:43:13 -0500

    still throwing undefined params error on stud page

--- src/components/StudentPageComp.jsx
+++ src/components/StudentPageComp.jsx
@@ -2,22 +2,35 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const StudentPageComp = () => {
-  const [students, setStudent] = useState([])
+  // const [period, setPeriod] = useState({})
+  const [student, setStudent] = useState([])
 
-  const getStudents = async () => {
-    const resp = await axios.get('https://localhost:5001/api/Student')
+  const getStudent = async () => {
+    const resp = await axios.get('https://localhost:5001/api/Student/')
     setStudent(resp.data)
+    console.log(resp.data)
   }
 
+  // const getPeriod = async () => {
+  //   const resp = await axios.get('https://localhost:5001/api/Period/')
+  //   setPeriod(resp.data.id)
+  // }
+
   useEffect(() => {
-    getStudents()
+    getStudent()
+    // getPeriod()
   }, [])
 
-  return students.map((st, i) => {
+  return student.map((st, i) => {
     return (
-      <p key={i}>
-        {st.name} is in {st.house}
-      </p>
+      <>
+        {/* <h2>
+          Period {period.periodNumber} - {period.subject}
+        </h2> */}
+        <p key={i}>
+          {st.name} is in {st.house}
+        </p>
+      </>
     )
   })
 }
